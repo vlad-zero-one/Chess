1ec5339014410973603b6069ee85a77cb2a9f892
vlad-zero-one <vladislav.igorevich.workmail@gmail.com>
2023-02-05 13:46:21 +0300

    DI, logger

---
+++ Assets/Scripts/Utils/Logger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using TMPro;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Logger : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI logText;
+    [SerializeField] private Button clear;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+
+        clear.onClick.AsObservable().Subscribe(_ => Clear()).AddTo(this);
+    }
+
+    private void Clear()
+    {
+        if (this == null || gameObject == null || logText == null) return;
+
+        logText.text = string.Empty;
+    }
+
+    public void Log<T>(T message)
+    {
+        if (this == null || gameObject == null || logText == null) return;
+
+        Debug.Log(message.ToString());
+
+        logText.text += message.ToString();
+        logText.text += "\n";
+    }
+}
